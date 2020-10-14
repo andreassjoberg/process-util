75b91e8fe20a623ac1652ba1ea679d7f2981ceb5
carllande <72446438+carllande@users.noreply.github.com>
2020-10-14 16:26:00 +0200

    Changes

--- src/ProcessUtil/Models/Project.cs
+++ src/ProcessUtil/Models/Project.cs
@@ -1,8 +1,10 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ProcessUtil.Models
 {
@@ -17,17 +19,43 @@ namespace ProcessUtil.Models
 
         public virtual void Start()
         {
-            // TODO: Implement
+            this.Status = Status.ACTIVE;
         }
 
         public virtual void Finish()
         {
-            // TODO: Implement
+
+
+            if (Status == Status.NEW || Status == Status.REMOVED)
+            {
+                throw new InvalidOperationException("Can't change status from Closed to `New` or `Removed`");
+            }
+
+            else if (Status == Status.ACTIVE)
+            {
+                foreach (var task in Tasks)
+                {
+                    if (task.Status != Status.CLOSED && task.Status != Status.REMOVED)
+                    {
+                        throw new InvalidOperationException("Can't close a porject with ");
+                    }
+
+                }
+
+                Status = Status.CLOSED;
+
+            }
+
         }
 
         public virtual void Remove()
         {
-            // TODO: Implement
+            if (Status == Status.NEW || Status == Status.ACTIVE)
+            {
+                throw new InvalidOperationException("Can't change status from Removed to `New` or `Active`");
+            }
+            else
+                Status = Status.REMOVED;
         }
     }
 }
